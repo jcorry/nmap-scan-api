bd35be927d25253d2ab20e36dd405a5ec5407eef
J Corry <jcorry@gmail.com>
2019-09-13 15:58:23 -0400

    5: Migrations consolidated into 2 files

---
+++ sql/migrations/000001_create_initial_structure.down.sql
@@ -0,0 +1,5 @@
+DROP TABLE addresses;
+DROP TABLE hostnames;
+DROP TABLE ports;
+DROP TABLE hosts;
+DROP TABLE imports;
--- sql/migrations/000004_create_hostnames_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE hostnames(
-    `id` INTEGER NOT NULL PRIMARY KEY,
-    `host_id` INTEGER NOT NULL,
-    `name` TEXT NOT NULL,
-    `type` TEXT NOT NULL,
-    FOREIGN KEY(host_id) REFERENCES hosts(id)
-);
